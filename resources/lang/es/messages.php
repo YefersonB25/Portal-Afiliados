33871858e49dbe55d857dac9436c59ec3da55752
Elkin Moreno <emoreno@tractocar.com>
2022-10-21 14:23:21 -0500

    lang

---
+++ resources/lang/es/messages.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+  "User error viewer"                                                                  => "Visualizador de errores de usuario",
+  "The requested page or action got a problem, which is described below"               => "La pagina o accion solicitada obtuvo un problema, el cual se describe acontinuacion",
+    "Location not found"                                                               => 'Ubicación no encontrada.',
+    "Customers (Account Site) not found"                                               => 'Clientes (sitio de cuenta) no encontrado',
+    "This option allows to massively load the OTs related to each referral"            => "Esta opcion permite cargar de forma masiva las OT relacionadas a cada una remision",
+    "Looks like you've got lost..."                                                    => "Parece que te has perdido ...",
+    "Upload invoices accounts receivable"                                              => "Cargar facturas cuentas a cobrar (notas credito)",
+  "Purchase shipments (payable) also known as manifests"                               => "Envíos de compra (por pagar) igualmente conocidos como manifiestos",
+  "They are commonly used for the accounts payable module"                             => "Son utilizados habitualmente para el módulo de cuentas por pagar",
+  "They are commonly used for the accounts receivable module"                          => "Son utilizados habitualmente para el módulo de cuentas por cobrar",
+  "All manifests are paid 15 days after the date of the compliment"                    => "Todos los manifiestos se pagan 15 días después de la fecha del cumplido",
+  "If the service provider receives Pay soon, it is canceled the same day"             => "Si el proveedor de servicios recibe Pronto Pago, se cancela el mismo día",
+  "Obtaining and updating the structures from the server was carried out successfully" => "La obtención y actualizacion de las estructuras desde el servidor se realizo con exito",
+  "Description Log Viewer"                                                             => "El visor de registros permite obtener información específica sobre ciertos eventos dentro del sistema. Permite entender al administrador del sistema, puede causar un error o mala configuración del sistema.",
+  "Limit Traslate"                                                                     => "Valor entero positivo que especifica el número máximo de elementos que devuelve un servidor. El servidor puede anular este valor para mejorar el rendimiento de la aplicación. Si la solicitud del cliente no especifica un valor límite, el servidor usa el valor límite predeterminado de 25. Este parámetro se aplica solo a los recursos de recopilación.",
+  "Offset Traslate"                                                                    => "Valor entero positivo que especifica el índice del primer elemento que se devolverá. El valor predeterminado es 0. Este parámetro solo se aplica a los recursos de recopilación. Por ejemplo:
+    Si offset                                                                          =  0, la respuesta contiene todos los recursos, comenzando desde el primer elemento de la colección.
+    Si offset                                                                          =  10, la respuesta contiene recursos a partir del undécimo elemento.",
+    "An SQL type error has been generated, check with the administrator"               => "Se ha generado un error tipo SQL, consulte con el administrador",
+    "You want to delete the selected record"                                           => "Desea eliminar el registro seleccionado",
+    "Upload credit notes to accounts receivable"                                       => "Subir notas crédito a cuentas por cobrar",
+    "You must select at least one order from the table"                                => "Debe seleccionar por lo menos un pedido de la tabla",
+    "An error occurred in the software logic. Check with your system administrator"    => "Ocurrió un error en la lógica del software. Consulte con el administrador de su sistema",
+    "Receivable Invoice successfully sent" => "Factura por cobrar enviada correctamente",
+    "Column creation is successful"    => "La creación de columnas sea realizado con éxito",
+    "The value for the attribute RemitToSupplier is invalid. You must provide a valid value. (AP-810667)" => "Configuracion de Pago a Tercero no valido. Revisar configuracion en el modulo de Proveedores ERP",
+    "You can't perform this operation because the value for the attribute Payment Method is required. (AP-810787)" => "No puede realizar esta operación porque el valor para el atributo Método de pago es obligatorio",
+    "This invoice number already exists. You must provide a unique invoice number. (AP-810776)" => "Numero de factura cuenta a pagar ya existe para ese proveedor"
+];
