1a6eb7b1341a695238bbd42c187a90e81266f615
Yeferson BC <yefersonbolano25@gmail.com>
2023-01-12 14:23:53 -0500

    correcciones

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -160,56 +160,6 @@
             </div>
         </div>
     </body>
-
-
-<div class="row">
-	<div class="col-lg-12 col-md-12">
-		<div class="card">
-			<div class="card-header border-bottom">
-				<h3 class="card-title">Supported Elements</h3>
-			</div>
-			<div class="card-body">
-				<form class="was-validated">
-					<div class="mb-3">
-						<label for="validationTextarea" class="form-label">Textarea</label>
-						<textarea class="form-control is-invalid" id="validationTextarea" placeholder="Required example textarea" required></textarea>
-					</div>
-
-					<div class="form-check mb-3">
-						<input type="checkbox" class="form-check-input" id="validationFormCheck1" required>
-						<label class="form-check-label" for="validationFormCheck1">Check this checkbox</label>
-					</div>
-
-					<div class="form-check">
-						<input type="radio" class="form-check-input" id="validationFormCheck2" name="radio-stacked" required>
-						<label class="form-check-label" for="validationFormCheck2">Toggle this radio</label>
-					</div>
-					<div class="form-check mb-3">
-						<input type="radio" class="form-check-input" id="validationFormCheck3" name="radio-stacked" required>
-						<label class="form-check-label" for="validationFormCheck3">Or toggle this other radio</label>
-					</div>
-
-					<div class="mb-3">
-						<select class="form-select" required aria-label="select example">
-						<option value="">Open this select menu</option>
-						<option value="1">One</option>
-						<option value="2">Two</option>
-						<option value="3">Three</option>
-						</select>
-					</div>
-
-					<div class="mb-3">
-						<input type="file" class="form-control" aria-label="file example" required>
-					</div>
-
-					<div class="mb-3">
-						<button class="btn btn-primary" type="submit" disabled>Submit form</button>
-					</div>
-					</form>
-			</div>
-		</div>
-	</div>
-</div>
 @endsection
 @section('scripts')
 {{-- <script>
